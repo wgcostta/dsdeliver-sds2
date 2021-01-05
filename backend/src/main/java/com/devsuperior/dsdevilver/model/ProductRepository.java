a3b2f749e4d1e0295d72caf237db9e546dd27cef
Wagner Costa <wg.o.costa@gmail.com>
2021-01-04 22:29:08 -0300

    Criado os repositórios

---
+++ backend/src/main/java/com/devsuperior/dsdevilver/model/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.devsuperior.dsdevilver.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Long,Product> {
+}
