9db4ff6fafdfede58c32e648115d9fa153d1a98e
Wagner Costa <wg.o.costa@gmail.com>
2021-01-04 21:41:12 -0300

    project created

---
+++ backend/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,65 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.*;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    //http://localhost:8080/swagger-ui.html
+    private static final String BASE_PACKAGE = "one.digitalinnovation.beerstock.controller";
+    private static final String API_TITLE = "Beer Stock API";
+    private static final String API_DESCRIPTION = "REST API for beer stock management";
+    private static final String CONTACT_NAME = "Rodrigo Peleias";
+    private static final String CONTACT_GITHUB = "https://gtihub.com/rpeleias";
+    private static final String CONTACT_EMAIL = "rodrigo.peleis@gmail.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
+    }
+
+//    private ApiInfo buildApiInfo() {
+//        return new ApiInfoBuilder()
+//                .title(API_TITLE)
+//                .description(API_DESCRIPTION)
+//                .version("1.0.0")
+//                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+//                .build();
+//    }
+
+    private List<ResponseMessage> responseMessageForGET()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+        }};
+    }
+}
