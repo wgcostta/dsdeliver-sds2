0a0ece555fe58928698c78b52fc355e1ce460043
Wagner Costa <wg.o.costa@gmail.com>
2021-01-07 00:56:12 -0300

    Criando componente Home

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+import { ReactComponent as MainImage } from './main.svg'
+import Footer from '../Footer'
+import { Link } from 'react-router-dom'
+
+function Home() {
+  return (
+    <>
+      
+      <Footer />
+    </>
+  )
+}
+
+export default Home
