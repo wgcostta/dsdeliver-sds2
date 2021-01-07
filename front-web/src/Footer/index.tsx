05f556980d9e2757475a38350ad576c6e40813a8
Wagner Costa <wg.o.costa@gmail.com>
2021-01-07 01:08:44 -0300

    Stilizando o Footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,7 +8,17 @@ function Footer() {
   return (
     <footer className="main-footer">
       App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
-      
+      <div className="footer-icons">
+        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+          <YouTubeIcon />
+        </a>
+        <a href="https://www.linkedin.com/school/devsuperior" target="_new">
+          <LinkedinIcon />
+        </a>
+        <a href="https://www.instagram.com/devsuperior.ig/">
+          <InstagramIcon />
+        </a> 
+      </div>
     </footer>
   )
 }
