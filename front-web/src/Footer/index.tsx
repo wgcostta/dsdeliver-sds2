c6bafa1c41d45ea4419d212f0723a3318383b32e
Wagner Costa <wg.o.costa@gmail.com>
2021-01-07 00:47:46 -0300

    EStilizando o primeiro componente

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css'
+import { ReactComponent as YouTubeIcon } from './youtube.svg'
+import { ReactComponent as LinkedinIcon } from './linkedin.svg'
+import { ReactComponent as InstagramIcon } from './instagram.svg'
+
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+      
+    </footer>
+  )
+}
+
+export default Footer
