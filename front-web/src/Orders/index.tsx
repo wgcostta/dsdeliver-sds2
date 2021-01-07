166cb72dc191f2fe116a5c7ff0dc520e2b5fb0f9
Wagner Costa <wg.o.costa@gmail.com>
2021-01-07 01:00:52 -0300

    Componente orders

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+import { fetchProducts } from '../api'
+import ProductsList from './ProductsList'
+import StepsHeader from './StepsHeader'
+import './styles.css'
+import { Product } from './types'
+
+function Orders() {
+
+}
+
+export default Orders
