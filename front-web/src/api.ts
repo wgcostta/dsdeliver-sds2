cc6da4736c7da8f05c218dc972c189a7b8cab81b
Wagner Costa <wg.o.costa@gmail.com>
2021-01-08 01:42:36 -0300

    Adicionando o codigo disponibilizado para consumir o Map

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const API_URL = 'https://devsuperior-2021.herokuapp.com'
+const mapboxToken = '';
 
 export function fetchProducts() {
   return axios(`${API_URL}/products`)
+}
+
+export function fetchLocalMapBox(local: string){
+  return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
 }
