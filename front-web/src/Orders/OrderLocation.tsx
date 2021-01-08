273808dad23b05ff58749d6e3832b228084c6689
Wagner Costa <wg.o.costa@gmail.com>
2021-01-08 01:27:30 -0300

    Adicionando o React Leaflet

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,16 +1,32 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+const position = {
+    lat: -22.6669475,
+    lng: -50.4204816
+}
+
 function OrderLocation() {
 
   return (
     <div className="order-location-container">
         <div className="order-location-content">
-        <h3 className="order-location-content">
-            Selecione onde o pedido deve ser entregue
-        </h3>
-        <div className="filter-container">
+            <h3 className="order-location-title">
+                Selecione onde o pedido deve ser entregue
+            </h3>
+            <div className="filter-container">
 
-        </div>
+            </div>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     </div>
   )
