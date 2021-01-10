c79e75c43df88df1f76920b5782bc2a964f2a5e9
Wagner Costa <wg.o.costa@gmail.com>
2021-01-10 15:59:22 -0300

    Criando o OrderDetails

---
+++ front-mobile/src/OrderDetails/index.tsx
@@ -0,0 +1,38 @@
+
+export default function OrderDetails({ route }: Props) {
+
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%'
+  },
+  button: {
+    backgroundColor: '#DA5C5C',
+    flexDirection: 'row',
+    borderRadius: 10,
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF',
+    letterSpacing: -0.24,
+    fontFamily: 'OpenSans_700Bold'
+  }
+});
--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./Home";
 import Orders from "./Orders";
+import OrderDetails from './OrderDetails';
 
 const Stack = createStackNavigator()
 
@@ -19,7 +20,8 @@ export default function Routes() {
       >
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Orders" component={Orders}/>
-    </Stack.Navigator>
+        <Stack.Screen name="OrderDetails" component={OrderDetails}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
