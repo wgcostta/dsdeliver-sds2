9721f7a1ff36264914449ec3b47e4af2006a818f
Wagner Costa <wg.o.costa@gmail.com>
2021-01-10 15:47:06 -0300

    Iniciando a requisição

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,13 +1,25 @@
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, ScrollView, Alert, Text, View } from 'react-native';
-
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
-
+import OrderCard from '../OrderCard';
 import { Order } from '../types';
 
 export default function Orders() {
-  
+  const [orders, setOrders] = useState<Order[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const navigation = useNavigation()
+  const isFocused = useIsFocused()
+
+  const fetchData = () => {
+    setIsLoading(true)
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+      .finally(() => setIsLoading(false))
+  }
 
 
   const handleOnPress = (order: Order) => {
@@ -19,6 +31,9 @@ export default function Orders() {
   return (
     <>
       <Header />
+      <ScrollView style={styles.container}>
+
+      </ScrollView>
     </>
   )
 
