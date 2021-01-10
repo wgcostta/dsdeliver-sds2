c2187863a91c06813d72a168b72f4f51fb0e2597
Wagner Costa <wg.o.costa@gmail.com>
2021-01-10 11:50:55 -0300

    Arquivos do projeto e imagens importadas

--- front-mobile/src/api.ts
+++ front-mobile/src/api.ts
@@ -2,3 +2,10 @@ import axios from "axios"
 
 const API_URL = 'https://devsuperior-2021.herokuapp.com'
 
+export function fetchOrders() {
+  return axios(`${API_URL}/orders`)
+} 
+
+export function confirmDelivery(orderId: number) {
+  return axios.put(`${API_URL}/orders/${orderId}/delivered`)
+}
