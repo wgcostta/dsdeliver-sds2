bc69503fbbcbec12b63e054260143bdb3005f833
Wagner Costa <wg.o.costa@gmail.com>
2021-01-10 11:49:56 -0300

    Adicionando os CSS disponibilizados

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -20,3 +20,21 @@ export default function Header() {
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#DA5C5C',
+    height: 90,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#FFF',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold'
+  }
+});
